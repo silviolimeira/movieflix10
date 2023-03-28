9fcb1b05ea6e22757ca31978d03a223a017076ed
Silvio Limeira <sicemal@gmail.com>
2023-03-28 19:28:12 -0400

    css layout navbar

---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,57 @@
+import { useContext, useEffect } from 'react';
+import history from 'util/history';
+import { Link } from 'react-router-dom';
+import { getTokenData, isAuthenticated } from 'util/auth';
+import { AuthContext } from 'AuthContext';
+import './styles.css';
+import { removeAuthData } from 'util/storage';
+
+const NavBar = () => {
+  const { authContextData, setAuthContextData } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAuthContextData({
+        authenticated: true,
+        tokenData: getTokenData(),
+      });
+    } else {
+      setAuthContextData({
+        authenticated: false,
+      });
+    }
+  }, [setAuthContextData]);
+
+  const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthContextData({
+      authenticated: false,
+    });
+    history.replace('/');
+  };
+  return (
+    <div className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="main-nav-logo-text">
+        <Link to="/">
+          <h4>MovieFlix</h4>
+        </Link>
+      </div>
+      <div className="main-nav-login-logout">
+        {authContextData.authenticated ? (
+          <>
+            <a href="#logout" onClick={handleLogoutClick}>
+              SAIR
+            </a>
+          </>
+        ) : (
+          <a href="#logout" onClick={handleLogoutClick}>
+            SAIR
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
