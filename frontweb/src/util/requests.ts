6e577bd400d1eea2b83c450a0c9eef8474a4f658
Silvio Limeira <sicemal@gmail.com>
2023-03-29 11:54:43 -0400

    fix test credentials

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -6,8 +6,8 @@ import { getAuthData } from './storage';
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
-const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'movieflix';
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'movieflix123';
 
 type LoginData = {
   username: string;
