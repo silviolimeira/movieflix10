42a5295a158ddd60955d3d23e48a47382aa9624c
Silvio Limeira <sicemal@gmail.com>
2023-03-31 23:13:22 -0400

    fix Home component with Movie, filter and pagination

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,11 +1,104 @@
+import { AxiosRequestConfig } from 'axios';
+import MovieCard from 'components/MovieCard';
+import GenreFilter, { MovieFilterData } from 'components/MovieCard/MovieFilter';
+import Pagination from 'components/Pagination';
+import { useCallback, useEffect, useState } from 'react';
+import { Movie } from 'types/movie';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
+type ControlComponentsData = {
+  activePage: number;
+  filterData: MovieFilterData;
+};
+
 const Home = () => {
+  const [page, setPage] = useState<SpringPage<Movie>>();
+
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+      filterData: { genre: null },
+    });
+
+  const handleSubmitFilter = (data: MovieFilterData) => {
+    setControlComponentsData({ activePage: 0, filterData: data });
+  };
+
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({
+      activePage: pageNumber,
+      filterData: controlComponentsData.filterData,
+    });
+  };
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getMovies = useCallback(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/movies',
+      withCredentials: true,
+      params: {
+        page: controlComponentsData.activePage,
+        size: 4,
+        genreId: controlComponentsData.filterData.genre?.id,
+      },
+    };
+
+    setIsLoading(true);
+    requestBackend(config)
+      .then((response) => {
+        setPage(response.data);
+        console.log('Home Movies: ', response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getMovies();
+  }, [getMovies]);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <div className="home-container-genre-filter">
+        <GenreFilter onSubmitFilter={handleSubmitFilter} />
+      </div>
+      <div className="row">
+        {page?.content.map((movie) => (
+          <div key={movie.id} className="col-sm-6 col-md-6 col-lg-6">
+            <MovieCard movie={movie} />
+          </div>
+        ))}
+      </div>
+      <Pagination
+        forcePage={page?.number}
+        pageCount={page ? page?.totalPages : 0}
+        range={3}
+        onChange={handlePageChange}
+      />
     </div>
   );
 };
 
 export default Home;
+
+/*
+      <GenreFilter onSubmitFilter={handleSubmitFilter} />
+      <div className="row">
+        {page?.content.map((movie) => (
+          <div key={movie.id} className="col-sm-6 col-md-12">
+            <h1>{movie.title}</h1>
+          </div>
+        ))}
+      </div>
+      <Pagination
+        forcePage={page?.number}
+        pageCount={page ? page?.totalPages : 0}
+        range={3}
+        onChange={handlePageChange}
+      />
+*/
